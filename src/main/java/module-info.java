52a3452a7a478035de8e3a2c476c0460b4837d0d
YahyaDalbah <yahya1dalbah@gmail.com>
2024-12-06 09:57:52 +0200

    initialize project

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module edu.najah.library {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens edu.najah.library to javafx.fxml;
+    exports edu.najah.library;
+}
