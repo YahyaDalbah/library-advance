2fa81f09aca17720b7121bbe0ee5d98e143c4b7c
YahyaDalbah <yahya1dalbah@gmail.com>
2024-12-12 14:17:37 +0200

    database structure

--- src/main/java/edu/najah/library/models/user/Student.java
+++ src/main/java/edu/najah/library/models/user/Student.java
@@ -2,8 +2,8 @@ package edu.najah.library.models.user;
 
 public class Student extends User {
 
-    public Student(String id, String name) {
-        super(id, name);
+    public Student(String name) {
+        super(name);
     }
 
 }
