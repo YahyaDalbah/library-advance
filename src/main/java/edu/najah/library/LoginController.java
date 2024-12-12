5b1c7e105594cb261c6456ddd0058523a3d61d2b
YahyaDalbah <yahya1dalbah@gmail.com>
2024-12-12 15:04:39 +0200

    added login as student

--- src/main/java/edu/najah/library/LoginController.java
+++ src/main/java/edu/najah/library/LoginController.java
@@ -1,25 +1,32 @@
 package edu.najah.library;
 
-import edu.najah.library.models.services.UserDAOImp;
-import edu.najah.library.models.user.Librarian;
-import edu.najah.library.models.user.User;
 import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class LoginController {
 
-    public void login(ActionEvent actionEvent) throws IOException {
+    public void login(ActionEvent event) throws IOException {
+        goToHomeScene(event);
+    }
+    public void loginAsStudent(MouseEvent event) throws IOException {
+        goToHomeScene(event);
+    }
+
+    private void goToHomeScene(Event event) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("home-clone.fxml"));
         Parent root = fxmlLoader.load();
-        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
+
 }
