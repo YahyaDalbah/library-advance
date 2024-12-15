1f948fa22d2f61836d90b85b1bddf69925958daa
Abdellatif Shtayeh <152719156+AbdellatifShtayeh@users.noreply.github.com>
2024-12-15 16:01:40 +0200

    Update MainApplication.java

--- src/main/java/edu/najah/library/MainApplication.java
+++ src/main/java/edu/najah/library/MainApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("Reservation-Book.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("login.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Library7");
         stage.setScene(scene);
